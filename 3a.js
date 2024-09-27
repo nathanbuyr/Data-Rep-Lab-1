5ce0521ac401e9c6e64b3b8ab291d0659147ed76
nathanbuyr <G00421082@atu.ie>
2024-09-27 14:19:00 +0000

    Beutified output

--- 3a.js
+++ 3a.js
@@ -1,9 +1,10 @@
 const names =["Nathan", "Karolis", "Jamie"];
 
 let addTask = (task)=>{
+    console.log("AddTask function:")
     names.push(task);
     console.log("Added " + task + " to the array.");
-    return console.log(names.length);
+    return console.log("Number of names in the array = " + names.length);
 }
 
 function listAllTasks(index){
@@ -13,9 +14,10 @@ function listAllTasks(index){
 let index = names.indexOf(names);
 
 let deleteTask = (task)=>{
+    console.log("DeleteTask function:")
     names.splice(index,1);
     console.log("Deleted " + task + " to the array.");
-    return console.log(names.length);
+    return console.log("Number of names in the array = " + names.length);
 }
 
 addTask("Jonathan");
